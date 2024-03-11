076de1afd05ae53b84459fbeab630195e37bb960
PMtHk <zooby88@naver.com>
2024-03-11 14:16:25 +0900

    feature : Notfound 페이지 추가
    - Notfound 컴포넌트 추가
    - Notfound 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/home'
 import MainLayout from './layouts/main'
+import NotFound from './pages/notfound'
 
 function App() {
   return (
@@ -9,11 +10,11 @@ function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/home" element={<Home />} />
+        {/* <Route path="/login" element="Login" /> */}
         <Route path="/puzzle" element="Puzzle" />
         <Route path="/puzzle/:id" element="Category" />
+        <Route path="*" element={<NotFound />} />
       </Route>
-      <Route path='/login'  element="Login" />
-      <Route path="*" element="Not Found" />
     </Routes>
   )
 }
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-[400px] space-y-4">
+      <div className="flex flex-col items-center justify-center space-y-4 text-center">
+        <h1 className="text-4xl font-bold tracking-tighter sm:text-6xl">
+          Page Not Found
+        </h1>
+        <p className="max-w-[600px] text-neutral-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          <span>찾으시는 페이지가 존재하지 않습니다.</span>
+          <br />
+          <span>
+            삭제되었거나 이름이 변경되었거나 일시적으로 사용할 수 없을 수
+            있습니다.
+          </span>
+        </p>
+      </div>
+      <Link
+        to="/home"
+        className="inline-flex h-10 items-center rounded-md border border-neutral-200 bg-neutral-50 px-8 text-sm font-medium shadow-sm transition-colors hover:bg-neutral-100 hover:text-neutral-900 "
+      >
+        홈으로 돌아가기
+      </Link>
+    </div>
+  )
+}
+
+export default NotFound

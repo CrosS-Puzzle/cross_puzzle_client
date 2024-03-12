603128d306d8e7237dbd9ee6ba45591c3b847ebd
PMtHk <zooby88@naver.com>
2024-03-12 13:06:40 +0900

    chore : react-query(tanstack query) 적용
    - react-query 도입

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import store from './store/index.ts'
 
@@ -10,12 +11,20 @@ import App from './App.tsx'
 
 import './index.css'
 
+const querytClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
+    <QueryClientProvider client={querytClient}>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>,
 )
